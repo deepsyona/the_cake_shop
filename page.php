031ed78082ff886508744b0acd7759d00185574d
deepsyona <deepsyonabhujel60@gmail.com>
2024-09-01 00:05:33 +0545

    Initial commit message

---
+++ page.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title> The Cake Shop | About Us</title>
+<!--Bootstrap -->
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets2/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="assets2/css/style.css" type="text/css">
+<link rel="stylesheet" href="assets2/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets2/css/owl.transitions.css" type="text/css">
+<link href="assets2/css/slick.css" rel="stylesheet">
+<link href="assets2/css/bootstrap-slider.min.css" rel="stylesheet">
+<link href="assets2/css/font-awesome.min.css" rel="stylesheet">
+
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets2/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets2/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets2/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets2/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets2/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!-- CSS -->
+<link href="style.css" type="text/css" rel="stylesheet" />
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+<link href='files/dist/themes/fontawesome-stars.css' rel='stylesheet' type='text/css'>
+   <!-- Bootstrap Core CSS -->
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    
+	    <!-- Customizable CSS -->
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+		<!-- Demo Purpose Only. Should be removed in production -->
+		<link rel="stylesheet" href="assets/css/config.css">
+
+		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+
+
+<!--Header-->
+<?php include('includes/top-header.php');?>
+<?php include('includes/main-header.php');?>
+<?php include('includes/menu-bar.php');?>
+<h4>The Cake Shop which is located at Kalopul, Kathmandu, is a beloved bakery that specializes in a variety of baked goods, from bread and pastries to custom cakes. Known for its quality products and friendly service, the owner’s engaging personality has made it a community favorite. We sell the best quality of Bakery products.  "We just don't make cakes, we bake happiness” Service A Sweet Cake and Gift is the perfect way to tell someone how much you love and care for them and how eagerly you are waiting to see them smile. All you need to have is an Internet connection to bring a smile to your loved ones. Moreover, Providing them Health Care, Hygenic Foods with the yummies tastes adds happiness to the life of your loved ones.</h4>
+
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer-->
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top-->
+
+<!--Login-Form -->
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/interface.js"></script>
+<!--bootstrap-slider-JS-->
+<script src="assets/js/bootstrap-slider.min.js"></script>
+<!--Slider-JS-->
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+
+</html>
